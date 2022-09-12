c06c81f07acaf889a1c4ee44d4201e77f32f5429
nbednar2929E <nbednar2929@charlottesvilleschools.org>
2022-09-12 15:55:21 -0400

    neopixel and rainow assignments

--- neopixel.py
+++ neopixel.py
@@ -1,10 +1,29 @@
 import board
 import neopixel
+import time
+import random 
 
 dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
-dot.brightness = 0.2 
+dot.brightness =  .1
 
 print("Make it red!")
 
+r = random.randint(0,255)
+g = random.randint(0,255)
+b = random.randint(0,255)
+rgb = [r,g,b]
+
+if (r>200):
+    g + b < 80
+
+if (g>200):
+    r + b < 80
+
+if (b>200):
+    g + r < 80
+
 while True:
-    dot.fill((255, 255, 255))
+    dot.fill((rgb))
+    time.sleep(.5)
+    dot.fill((0,0,0))
+    time.sleep(0.5)
