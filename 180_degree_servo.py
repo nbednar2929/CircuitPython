fa09da5c39b5728f02dd6980ebcab195a6fd9f8c
nbednar2929E <nbednar2929@charlottesvilleschools.org>
2022-09-13 15:20:06 -0400

    commenting and minor fixes

--- 180_degree_servo.py
+++ 180_degree_servo.py
@@ -1,10 +1,10 @@
-#Nick
+#Nick Bednar
 #9/13/22
 #Servo shifts between 0-180 degrees back and forth.
 import time
 import board
 import pwmio
-from adafruit_motor import servo
+from adafruit_motor import motor
 
 # create a PWMOut object on Pin 9.
 pwm = pwmio.PWMOut(board.D9, duty_cycle=2 ** 20, frequency=40)
@@ -13,9 +13,9 @@ pwm = pwmio.PWMOut(board.D9, duty_cycle=2 ** 20, frequency=40)
 my_servo = servo.Servo(pwm)
 
 while True:
-    for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
+    for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time forward.
         my_servo.angle = angle
         time.sleep(0.05)
-    for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
+    for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time backward.
         my_servo.angle = angle
         time.sleep(0.05)
--- neopixel.py
+++ neopixel.py
@@ -3,27 +3,27 @@ import neopixel
 import time
 import random 
 
-dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
-dot.brightness =  .1
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1) #creates name(dot) for neopixels
+dot.brightness =  .1 #sets neopixel brightness
 
 print("Make it red!")
 
-r = random.randint(0,255)
-g = random.randint(0,255)
-b = random.randint(0,255)
-rgb = [r,g,b]
+r = random.randint(0,255) #random integer between 0-225 for r(red)
+g = random.randint(0,255) #random integer between 0-225 for g(green)
+b = random.randint(0,255) #random integer between 0-225 for b(blue)
+rgb = [r,g,b] #sets rgb value for random integers
 
-if (r>200):
+if (r>200): #if r>200 g+b<80 in order to get more solid colors, less white
     g + b < 80
 
-if (g>200):
+if (g>200): #same principle except for green
     r + b < 80
 
-if (b>200):
+if (b>200): #same principle for these two except for blue
     g + r < 80
 
 while True:
-    dot.fill((rgb))
+    dot.fill((rgb)) #make led randomly assigned rgb color
     time.sleep(.5)
-    dot.fill((0,0,0))
+    dot.fill((0,0,0)) 
     time.sleep(0.5)
