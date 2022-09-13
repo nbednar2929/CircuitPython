72e831819cc799a49dfd3bd5dd8f1c22bc432e5d
nbednar2929E <nbednar2929@charlottesvilleschools.org>
2022-09-13 15:14:54 -0400

    servo assignment

---
+++ 180_degree_servo.py
@@ -0,0 +1,21 @@
+#Nick
+#9/13/22
+#Servo shifts between 0-180 degrees back and forth.
+import time
+import board
+import pwmio
+from adafruit_motor import servo
+
+# create a PWMOut object on Pin 9.
+pwm = pwmio.PWMOut(board.D9, duty_cycle=2 ** 20, frequency=40)
+
+# Create a servo object, my_servo.
+my_servo = servo.Servo(pwm)
+
+while True:
+    for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
+        my_servo.angle = angle
+        time.sleep(0.05)
+    for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
+        my_servo.angle = angle
+        time.sleep(0.05)
