9de4c34f25a0efe8ad69b018cd10caeaafb6c3d9
nbednar2929E <nbednar2929@charlottesvilleschools.org>
2022-09-29 15:35:43 -0400

    resubmit servo assignment

--- 180_degree_servo.py
+++ 180_degree_servo.py
@@ -2,17 +2,28 @@ import time #importing files
 import board
 import pwmio
 from adafruit_motor import servo
+from digitalio import DigitalInOut,Direction,Pull
+import simpleio
 
 # create a PWMOut object on Pin 9.
-pwm = pwmio.PWMOut(board.D9, duty_cycle=2 ** 20, frequency=40)
+pwm = pwmio.PWMOut(board.D7, duty_cycle=2 ** 20, frequency=40)
 
 # Create a servo object, my_servo.
 my_servo = servo.Servo(pwm)
 
+btn = DigitalInOut(board.D4) #create button 1
+btn2 = DigitalInOut(board.D3) #create button 2
+btn.direction = Direction.INPUT #create button 1 direction
+btn.pull = Pull.UP #pull up button 1
+btn2.direction = Direction.INPUT #create button 2 direction
+btn2.pull = Pull.UP #pull up button 2
+
 while True:
-    for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time forward.
-        my_servo.angle = angle
-        time.sleep(0.05)
-    for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time backward.
-        my_servo.angle = angle
-        time.sleep(0.05)
+    if not btn.value: #button 1 being pressed
+        for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time forward.
+            my_servo.angle = angle
+            time.sleep(0.05)
+    if not btn2.value: #button 2 being pressed
+        for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time backward.
+            my_servo.angle = angle
+            time.sleep(0.05)
--- lcd_screen.py
+++ lcd_screen.py
@@ -1,4 +1,4 @@
-import board
+import board #imoprt all files
 from lcd.lcd import LCD
 from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
 import time
@@ -6,35 +6,39 @@ from digitalio import DigitalInOut, Direction, Pull
 from analogio import AnalogIn
 import simpleio
 
-i2c = board.I2C() #get an i2c object
-lcd = LCD(I2CPCF8574Interface(i2c, 0x27), num_rows=2, num_cols=16)
-ptmr = AnalogIn(board.A1)
+i2c = board.I2C() #get an i2c object 
+lcd = LCD(I2CPCF8574Interface(i2c, 0x27), num_rows=2, num_cols=16) #initiate LCD screen
+ptmr = AnalogIn(board.A1) #identify potentiometer
 
-btn = DigitalInOut(board.D7)
-btn.direction = Direction.INPUT
-btn.pull = Pull.UP
-counter = 0
-
+btn = DigitalInOut(board.D7) #identify button
+btn.direction = Direction.INPUT #identify button direction
+btn.pull = Pull.UP #pull button up
+counter = 0 #initialize counter
+delta = 1 #initialize variable that counter changes by
+lastBtn = btn.value #creates a second button value variable to call back to
 
 while True:
-    print(ptmr.value)
-    if ptmr.value<47000 and not btn.value:
-        lcd.set_cursor_pos(0,0)
-        lcd.print("BTN State: Down  ")
-        lcd.set_cursor_pos(1,0)
-        lcd.print('Presses:')
-        lcd.set_cursor_pos(1,12)
-        lcd.print(str(counter))
-        counter = counter-1            
+    print(ptmr.value) #print potentiometer value
+    lcd.set_cursor_pos(1,0) #set lcd cursor
+    lcd.print(" Presses:") #print presses
+    lcd.set_cursor_pos(1,12) #set lcd cursor
+    lcd.print(str(counter)) #print counter
+    lcd.print(" ") #removes excess integers after going between 9-11
+   
+    
+    if ptmr.value<47000: #if potentiometer reads less than 47,000
+        lcd.set_cursor_pos(0,0) #set cursor on lcd
+        lcd.print("BTN State: Down  ") #print button state: down
+        delta = -1 #set delta to -1
     else:
-        lcd.set_cursor_pos(0,0)
-        lcd.print("BTN State: Up   ")
-        lcd.set_cursor_pos(1,0)
-        lcd.print('Presses:')
-        lcd.set_cursor_pos(1,12)
-        lcd.print(str(counter))
-        counter = counter+1
+        lcd.set_cursor_pos(0,0) #set lcd cursor
+        lcd.print("BTN State: UP    ") #print button state up
+        delta = 1 #set delta to 1
+    
+    if not btn.value and lastBtn != btn.value: #if button is being pressed and button value is not the same as it was prior
+        counter = counter + delta #add delta to counter
 
-    time.sleep(0.5)
+    lastBtn = btn.value #restating lastBtn
+    time.sleep(0.05)
 #problems: counts without button press
 #zeros stay on the lcd when counting back from ten
