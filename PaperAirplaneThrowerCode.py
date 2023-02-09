bd18271df14c1edbbd33c7315bf86dd17b1b8b0f
nbednar2929E <nbednar2929@charlottesvilleschools.org>
2023-02-09 16:00:19 -0500

    sadness

---
+++ PaperAirplaneThrowerCode.py
@@ -0,0 +1,27 @@
+import time #importing files
+import board
+import digitalio
+import pwmio
+from adafruit_motor import servo
+from digitalio import DigitalInOut,Direction,Pull
+import simpleio
+
+# create a PWMOut object on Pin 9.
+pwm = pwmio.PWMOut(board.D10, duty_cycle=2 ** 20, frequency=40)
+
+# Create a servo object, my_servo.
+my_servo = servo.Servo(pwm)
+
+btn = DigitalInOut(board.D8) #create button 
+btn.direction = Direction.INPUT #identify button  direction
+btn.pull = Pull.UP #pull up button 
+
+switch = DigitalInOut(board.D12) #create switch
+switch.direction = Direction.INPUT #identify button direciton
+
+while True:
+    if switch.value: #if switch is on
+        if btn.value: #if button being pressed
+            for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time forward.
+                my_servo.angle = angle
+                time.sleep(0.05)
