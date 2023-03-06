31a11010104e1ec8cf98fa256fd60288f3faf260
nbednar2929E <nbednar2929@charlottesvilleschools.org>
2023-03-06 15:08:37 -0500

    sagdf

--- PaperAirplaneThrowerCode.py
+++ PaperAirplaneThrowerCode.py
@@ -20,8 +20,12 @@ switch = DigitalInOut(board.D12) #create switch
 switch.direction = Direction.INPUT #identify button direciton
 
 while True:
-    if switch.value: #if switch is on
+    if True: #switch.value: #if switch is on
+        print('Switch1')
+        time.sleep(0.5)
         if btn.value: #if button being pressed
+            print('Button')
             for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time forward.
                 my_servo.angle = angle
                 time.sleep(0.05)
+                print(angle)
