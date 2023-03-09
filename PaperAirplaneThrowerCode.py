bdd4e86bca6ce9d0e33aaa0c69f149bf376fe73e
nbednar2929E <nbednar2929@charlottesvilleschools.org>
2023-03-09 16:03:20 -0500

    asdfafds

--- PaperAirplaneThrowerCode.py
+++ PaperAirplaneThrowerCode.py
@@ -12,20 +12,19 @@ pwm = pwmio.PWMOut(board.D10, duty_cycle=2 ** 20, frequency=40)
 # Create a servo object, my_servo.
 my_servo = servo.Servo(pwm)
 
-btn = DigitalInOut(board.D8) #create button 
+btn = DigitalInOut(board.D13) #create button 
 btn.direction = Direction.INPUT #identify button  direction
-btn.pull = Pull.UP #pull up button 
+btn.pull = Pull.DOWN #pull up button 
 
-switch = DigitalInOut(board.D12) #create switch
-switch.direction = Direction.INPUT #identify button direciton
+
 
 while True:
-    if True: #switch.value: #if switch is on
-        print('Switch1')
-        time.sleep(0.5)
-        if btn.value: #if button being pressed
-            print('Button')
-            for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time forward.
-                my_servo.angle = angle
-                time.sleep(0.05)
-                print(angle)
+    print('Switch1')
+    time.sleep(0.1)
+    if btn.value: #if button being pressed
+        print('Button')
+        for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time forward.
+            my_servo.angle = angle
+            print(angle)
+        for angle in range(180, 0, -5):  # 0 - 180 degrees, 5 degrees at a time forward.
+            my_servo.angle = angle
